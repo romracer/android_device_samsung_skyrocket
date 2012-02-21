e72066b7a473cab370c5583dc2b1c075e678a65c
Scott <romracer@gmail.com>
2012-02-21 13:27:35 -0600

    ALL_PREBUILT isn't allowed anymore

--- device_hercules.mk
+++ device_hercules.mk
@@ -31,7 +31,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ueventd.rc:root/ueventd.rc \
     $(LOCAL_PATH)/init.qcom.lpm_boot.sh:root/init.qcom.lpm_boot.sh \
     $(LOCAL_PATH)/init.qcom.sh:root/init.qcom.sh \
-    $(LOCAL_PATH)/lpm.rc:root/lpm.rc
+    $(LOCAL_PATH)/lpm.rc:root/lpm.rc \
+    $(LOCAL_PATH)/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := device/samsung/hercules/kernel
