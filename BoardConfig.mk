9eb455eaf596c049cf0d9c896af95fa0fc2d3164
Scott <romracer@gmail.com>
2012-04-30 11:20:03 -0500

    Revert kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,7 +53,7 @@ BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/skyrocket/recovery/graphics.c
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/skyrocket/recovery/recovery_keys.c
 BOARD_CUSTOM_RECOVERY_UI := ../../device/samsung/skyrocket/recovery/recovery_ui.c
 
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom usb_id_pin_rework=true
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_watchdog.appsbark=0 msm_watchdog.enable=1 loglevel=4
 BOARD_KERNEL_BASE := 0x40400000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_FORCE_RAMDISK_ADDRESS := 0x41900000
